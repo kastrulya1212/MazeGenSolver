486c7d5c088f1f8973e3141a71cefe34be2ae343
kastrulya1212 <9154758163@mail.ru>
2024-11-15 16:53:00 +0300

    Right hand solving algorithm.

--- debug.py
+++ debug.py
@@ -1,16 +1,82 @@
+# import random
+#
+# class Test:
+#     def __init__(self, mass, heigth):
+#         self.mass = mass
+#         self.height = heigth
+#     def update(self):
+#         self.__init__(50,7.5)
+#
+# a1 = Test(100, 15)
+# a1.update()
+# print(a1.mass, a1.height)
+#
+# a = [1,2,3]
+# a.pop(-1)
+# print(a)
+
+import pygame
+import sys
 import random
 
-class Test:
-    def __init__(self, mass, heigth):
-        self.mass = mass
-        self.height = heigth
-    def update(self):
-        self.__init__(50,7.5)
+# INITIALIZATION SETTINGS
+HEIGHT = 480
+WIDTH = 480
+FPS = 5
+W = 5
 
-a1 = Test(100, 15)
-a1.update()
-print(a1.mass, a1.height)
 
-a = [1,2,3]
-a.pop(-1)
-print(a)
+
+GREEN = (0, 255, 0)
+DARK_GREEN = (0, 100, 0)
+LIGHT_GREEN = (30, 150, 30)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+class Cell:
+    def __init__(self, rect, color):
+        self.rect = rect
+        self.color = color
+    def draw_lines(self):
+        left_top = self.rect.topleft
+        left_bottom = self.rect.bottomleft
+        right_bottom = self.rect.bottomright
+        right_top = self.rect.topright
+
+        pygame.draw.line(screen, GREEN, left_top, right_top, W)
+        pygame.draw.line(screen, GREEN, left_top, left_bottom, W)
+        pygame.draw.line(screen, GREEN, right_bottom, left_bottom, W)
+        pygame.draw.line(screen, GREEN, right_bottom, right_top, W)
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color, self.rect)
+        self.draw_lines()
+
+def main():
+    # start settings
+    running = True
+    screen.fill(BLACK)
+
+    rect1 = pygame.Rect(100, 100, 100, 100)
+    rect2 = pygame.Rect(300, 100, 100, 100)
+    a1 = Cell(rect1, DARK_GREEN); a2 = Cell(rect2, LIGHT_GREEN)
+    a1.draw(); a2.draw();
+
+    # screen update loop
+    while running:
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+    #---# CODE HERE
+
+    #---# CODE STOP
+        pygame.display.update()
+        clock.tick(FPS)
+#-----------------------------------------------
+main()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from reprlib import recursive_repr
+import time
 
 import pygame
 import sys
@@ -7,11 +7,11 @@ import random
 # INITIALIZATION SETTINGS
 HEIGHT = 480
 WIDTH = 480
-FPS = 5
-SIZE = 30    # Cell size(square)
-W = 5        # Outline thickness
-ROWS = 10    #WIDTH // SIZE
-COLS = 15    #HEIGHT // SIZE
+FPS = 20
+SIZE = 20    # Cell size(square)
+W = 4        # Outline thickness
+ROWS = 20    # WIDTH // SIZE
+COLS = 20    # HEIGHT // SIZE
 cells = []
 
 GREEN = (0, 255, 0)
@@ -54,35 +54,64 @@ class Explorer:
     def explorer_draw(self):
         pygame.draw.rect(screen, WHITE, (self.col*SIZE+W/2, self.row*SIZE+W/2, SIZE-W/2, SIZE-W/2))
 
+    def init_simple(self, cell: Cell):
+        self.row = cell.row
+        self.col = cell.col
+        self.cell = cell
+
     def move_right(self):
         self.cell.lines[2] = 0
         new_cell = cells[self.col+1][self.row]
         self.__init__(new_cell)
         self.cell.lines[1] = 0
         self.cell.visited = 1
-        self.explorer_draw()
+        #self.explorer_draw()
     def move_left(self):
         self.cell.lines[1] = 0
         new_cell = cells[self.col-1][self.row]
         self.__init__(new_cell)
         self.cell.lines[2] = 0
         self.cell.visited = 1
-        self.explorer_draw()
+        #self.explorer_draw()
     def move_up(self):
         self.cell.lines[0] = 0
         new_cell = cells[self.col][self.row-1]
         self.__init__(new_cell)
         self.cell.lines[3] = 0
         self.cell.visited = 1
-        self.explorer_draw()
+        #self.explorer_draw()
     def move_down(self):
         self.cell.lines[3] = 0
         new_cell = cells[self.col][self.row+1]
         self.__init__(new_cell)
         self.cell.lines[0] = 0
         self.cell.visited = 1
-        self.explorer_draw()
+        #self.explorer_draw()
 
+    def move_right_common(self):
+        new_cell = cells[self.col + 1][self.row]
+        #self.__init__(new_cell)
+        self.init_simple(new_cell)
+        #self.explorer_draw()
+    def move_left_common(self):
+            new_cell = cells[self.col - 1][self.row]
+            #self.__init__(new_cell)
+            self.init_simple(new_cell)
+            #self.explorer_draw()
+    def move_up_common(self):
+        new_cell = cells[self.col][self.row - 1]
+        #self.__init__(new_cell)
+        self.init_simple(new_cell)
+        #self.explorer_draw()
+    def move_down_common(self):
+            new_cell = cells[self.col][self.row + 1]
+            #self.__init__(new_cell)
+            self.init_simple(new_cell)
+            #self.explorer_draw()
+
+
+# StartCell --> CurrIsVisible --> CheckNeighbour --> RandomUnvisitedCell -->
+# --> DeleteWall_Curr_and_Neighbour --> CurrCell=Neighbour
 class Recursion_Generartion(Explorer):
     def __init__(self, cell: Cell):
         super().__init__(cell)
@@ -149,7 +178,92 @@ class Recursion_Generartion(Explorer):
             self.path_searching()
             if self.path != []:
                 self.path_step_back()
-        print("Done!", "Iterations:", k)
+        print("Creating done!", "Iterations:", k)
+
+# RightHand_Solver(start_cell, *start_Direction)
+# direction is direction TO NEXT CELL
+class RightHand_Solver(Explorer):
+    def __init__(self, cell:Cell, end_cell:Cell, direction=0, steps=0):
+        super().__init__(cell)
+        try: self.path = self.path
+        except: self.path = []
+        self.direction = direction
+        self.end_cell = end_cell
+        self.steps = steps
+
+    def counterclockwise_next(self, num):
+        return (num+1)%4
+    def clockwise_next(self, num):
+        return (num-1)%4
+
+    def goto_cell(self, next_cell):
+        # attempt without new val
+        rowN, colN = next_cell.row, next_cell.col
+        if self.row == rowN:
+            if self.col-1 == colN:
+                self.move_left_common()
+            elif self.col+1 == colN:
+                self.move_right_common()
+        elif self.col == colN:
+            if self.row-1 == rowN:
+                self.move_up_common()
+            elif self.row+1 == rowN:
+                self.move_down_common()
+
+    def clockind_to_ind(self, num):
+        if num==0: return 0
+        elif num==1: return 1
+        elif num==2: return 3
+        elif num==3: return 2
+
+
+    def diretcrion_from(self, cellHome:Cell, cellCur:Cell):
+        if (cellCur.row-1)==(cellHome.row): return
+
+    # return clocksort array of allowed cells around
+    # example: [0, 0, Cell_obj, 0] means that bottom is available
+    def allowed_cells_clocksort(self):
+        # m[0]=top, m[1]=left, m[2]=right, m[3]=bottom
+        allowed_directions = self.cell.lines
+        # m[0]=top, m[1]=left, m[2]=bottom, m[3]=right
+        # 0=None OR cell:Cell
+        allowed_cells_clocksort = [0,0,0,0]
+        for i in range(0,4):
+            if allowed_directions[i]==0:
+                if i==0:
+                    allowed_cells_clocksort[0] = (cells[self.col][self.row-1])
+                elif i==1:
+                    allowed_cells_clocksort[1] = (cells[self.col-1][self.row])
+                elif i==2:
+                    allowed_cells_clocksort[3] = (cells[self.col+1][self.row])
+                elif i==3:
+                    allowed_cells_clocksort[2] = (cells[self.col][self.row+1])
+        return allowed_cells_clocksort
+
+    def right_algorithm_step(self):
+        # STEPS:
+        # *0) start direction = any(0 for example)
+        # 1) rotate direction arrow counterclockwise (search allowed lines)
+        # 2) go to this closest to counterclockwise cell
+        allowed_clocksort = self.allowed_cells_clocksort()
+        next_counterclock_ind = self.clockwise_next(self.direction)
+
+        # searching in counterclockwise
+        while allowed_clocksort[next_counterclock_ind] == 0:
+            next_counterclock_ind = self.counterclockwise_next(next_counterclock_ind)
+        # next cell is found!
+        if (self.cell not in self.path): self.path.append(self.cell)
+        #self.direction = self.clockind_to_ind(next_counterclock_ind)
+        self.direction = next_counterclock_ind
+        self.goto_cell(allowed_clocksort[next_counterclock_ind])
+        self.cell.visited = 1; self.steps += 1
+
+    def full_search(self):
+        k = 0
+        while self.cell != self.end_cell:
+            self.right_algorithm_step()
+            k += 1
+        print("Solving done!", f'Iterations: {k}')
 
 
 def start_data():
@@ -176,7 +290,7 @@ def border():
 
 def main():
     # start settings
-    running = True
+    running = True; find_end = True
     screen.fill(BLACK)
     start_data()
 
@@ -187,6 +301,10 @@ def main():
 
     dot = Recursion_Generartion(start_cell)
     dot.recursion_generation()
+    # Maze has been generated
+
+    solver = RightHand_Solver(start_cell, end_cell)
+    #solver.full_search()
 
     # dot.move_right(); dot.move_down(); dot.move_right(); dot.move_down()
     # dot.move_left(); dot.move_left(); dot.move_up(); dot.move_down(); dot.move_down()
@@ -194,8 +312,11 @@ def main():
     # start cells grid
     for _col in cells:
         for cell in _col:
+            cell.visited = 0
             cell.draw()
-    dot.explorer_draw()
+            #solver.explorer_draw()
+    start_cell.visited = 1
+    pygame.display.update()
 
     # screen update loop
     while running:
@@ -206,20 +327,35 @@ def main():
                 pygame.quit()
                 sys.exit()
     #---# CODE HERE
+        for _col in cells:
+            for cell in _col:
+                cell.draw()
 
+        solver.explorer_draw()
+
+        if solver.cell != end_cell:
+            solver.right_algorithm_step()
+        else:
+            if find_end == True: print(f"Solving done! Iterations: {solver.steps}")
+            find_end = False
+        pygame.display.update()
+        #time.sleep(0.01)
 
 
     #---# CODE STOP
-        pygame.display.update()
+        #pygame.display.update()
         clock.tick(FPS)
 #-----------------------------------------------
 main()
 
+# DO NOT FORGET:
+# 1) change "visible" system in creating mode (use "visible" attribute only in searching mode)
+# !err 2) __init__ wanna take second argument end_cell
 
 # TODO LIST (IDEAS):
-# 1) Cell update synchronized with screen update (vizualization)
+# in process 1) Cell update synchronized with screen update (vizualization)
 # 2) New maze generation methods
-# 3) Maze solvers
+# in process 3) Maze solvers
 # 4) Create user keyboard "explorer" control
 
 
